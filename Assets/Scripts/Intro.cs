391f04b92fa33fe4139022712667f5c352a15fce
spindlyjohnny <saifulfarhan05@gmail.com>
2023-05-02 16:07:55 +0800

    intro load tutorial
    added a script to load tutorial

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public float waittime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator LoadScene() {
+        yield return new WaitForSeconds(waittime);
+        SceneManager.LoadScene("Tutorial");
+    }
+}
