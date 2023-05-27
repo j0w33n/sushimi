d372f4985c7acaff7115a8b9f2411a70ad0d5bbd
darrenistic <darren2205@gmail.com>
2023-05-28 04:33:02 +0800

    Updated Projectile Trail, Enhanced DMG indicators,  Fixed Bullet Projectile Rotation ETC

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -28,9 +28,11 @@ public class ProjectileScript : MonoBehaviour
     protected virtual void Update()
     {
         Destroy(gameObject, projectilelife);
-        Vector2 lookdir = shootingScript.joystickposition - rb.position;
-        float angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+        Vector2 joystickPosition = shootingScript.joystickposition.normalized;
+        if (joystickPosition != Vector2.zero) {
+            float angle = Mathf.Atan2(joystickPosition.y, joystickPosition.x) * Mathf.Rad2Deg - 90f;
+            rb.rotation = angle + 90;
+        }
     }
    public virtual void Move() {
         rb.velocity = shootingScript.joystickposition.normalized * speed;
