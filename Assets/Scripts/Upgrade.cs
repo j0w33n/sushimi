feb64130760db11d541a2850c87aa501adfb1e72
spindlyjohnny <saifulfarhan05@gmail.com>
2023-06-13 20:04:25 +0800

    Upgrade switching works?

--- Assets/Scripts/Upgrade.cs
+++ Assets/Scripts/Upgrade.cs
@@ -9,64 +9,11 @@ public class Upgrade : MonoBehaviour
     Weapon[] shooting;
     LevelManager levelManager;
     UpgradePanel upgradePanel;
+    bool switchupgrade;
     public Image activebuff;
-   /* [System.Serializable]
-    public class UpgradeData {
-        public UpgradeType upgradeType;
-        public float floatValue;
-        public int intValue;
-        public bool boolValue;
-    }
-
-    public enum UpgradeType {
-        IncreaseFireRate,
-        IncreaseHealth,
-        IncreaseMaxAmmo,
-        IncreaseDamage,
-        DoubleBarrelGun,
-        SlowingBullets,
-        ExplodingBullets
-    }
-
-    public List<UpgradeData> upgrades = new List<UpgradeData>();
-    private int currentUpgradeIndex = 0;
-    // Switch to the next upgrade
-    public void SwitchUpgrade() {
-        currentUpgradeIndex++;
-        if (currentUpgradeIndex >= upgrades.Count)
-            currentUpgradeIndex = 0;
-
-        ApplyUpgrade(upgrades[currentUpgradeIndex]);
-    }
-    private void ApplyUpgrade(UpgradeData upgrade) {
-        switch (upgrade.upgradeType) {
-            case UpgradeType.IncreaseFireRate:
-                IncreaseFireRate(upgrade.floatValue);
-                break;
-            case UpgradeType.IncreaseHealth:
-                IncreaseHealth(upgrade.floatValue);
-                break;
-            case UpgradeType.IncreaseMaxAmmo:
-                IncreaseMaxAmmo(upgrade.intValue);
-                break;
-            case UpgradeType.IncreaseDamage:
-                IncreaseDamage(upgrade.intValue);
-                break;
-            case UpgradeType.DoubleBarrelGun:
-                DoubleBarrelGun();
-                break;
-            case UpgradeType.SlowingBullets:
-                SlowingBullets();
-                break;
-            case UpgradeType.ExplodingBullets:
-                ExplodingBullets();
-                break;
-        }
-        upgradePanel.active = false;
-        activebuff.color = new Color(activebuff.color.r, activebuff.color.g, activebuff.color.b, 1);
-        activebuff.sprite = GetComponent<Image>().sprite;
-    }*/
     public void IncreaseFireRate(float value) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
         foreach(var i in shooting) {
             i.firerate -= value;
             i.reloadspeed += value;
@@ -76,35 +23,47 @@ public class Upgrade : MonoBehaviour
         }
     }
     public void IncreaseHealth(float value) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
         player.maxhitpoints += value;
         player.hitpoints = player.maxhitpoints;
         RectTransform rect = player.healthbar.fillRect.GetComponent<RectTransform>();
         RectTransform fillarea = player.healthbar.fillRect.GetComponentInParent<RectTransform>();
         RectTransform bg = player.healthbar.GetComponentInChildren<Image>().GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2(rect.sizeDelta.x + value * 2, rect.sizeDelta.y);
-        fillarea.right = new Vector3(fillarea.right.x + value, fillarea.right.y, fillarea.right.z);
-        bg.sizeDelta = new Vector2(bg.sizeDelta.x + value, bg.sizeDelta.y);
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        fillarea.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        bg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        //rect.sizeDelta = new Vector2(rect.sizeDelta.x + value * 2, rect.sizeDelta.y);
+        //fillarea.right = new Vector3(fillarea.right.x + value * 2, fillarea.right.y, fillarea.right.z);
+        //bg.sizeDelta = new Vector2(bg.sizeDelta.x + value * 2, bg.sizeDelta.y);
         upgradePanel.active = false;
         activebuff.color = new Color(activebuff.color.r, activebuff.color.g, activebuff.color.b, 1);
         activebuff.sprite = GetComponent<Image>().sprite;
     }
     public void IncreaseMaxAmmo(int value) {
-        foreach(var i in shooting) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
+        foreach (var i in shooting) {
             i.maxammo += value;
             i.ammo = i.maxammo;
         }
         RectTransform rect = levelManager.ammobar.fillRect.GetComponent<RectTransform>();
         RectTransform fillarea = levelManager.ammobar.fillRect.GetComponentInParent<RectTransform>();
         RectTransform bg = levelManager.ammobar.fillRect.GetComponentInChildren<Image>().GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2(rect.sizeDelta.x + value * 2, rect.sizeDelta.y);
-        fillarea.right = new Vector3(fillarea.right.x + value, fillarea.right.y, fillarea.right.z);
-        bg.sizeDelta = new Vector2(bg.sizeDelta.x + value, bg.sizeDelta.y);
+        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        fillarea.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        bg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, value);
+        /*rect.sizeDelta = new Vector2(rect.sizeDelta.x + value * 2, rect.sizeDelta.y);
+        fillarea.right = new Vector3(fillarea.right.x + value * 2, fillarea.right.y, fillarea.right.z);
+        bg.sizeDelta = new Vector2(bg.sizeDelta.x + value * 2, bg.sizeDelta.y);*/
         upgradePanel.active = false;
         activebuff.color = new Color(activebuff.color.r, activebuff.color.g, activebuff.color.b, 1);
         activebuff.sprite = GetComponent<Image>().sprite;
     }
     public void IncreaseDamage(int value) {
-        foreach(var i in shooting) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
+        foreach (var i in shooting) {
             i.projectileprefab.GetComponent<ProjectileScript>().damage += value;
             i.explodingprojectile.GetComponent<ProjectileScript>().damage += value;
             i.slowingprojectile.GetComponent<ProjectileScript>().damage += value;
@@ -117,13 +76,17 @@ public class Upgrade : MonoBehaviour
         activebuff.sprite = GetComponent<Image>().sprite;
     }
     public void DoubleBarrelGun() {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
         player.SwitchWeapon(1);
         upgradePanel.active = false;
         activebuff.color = new Color(activebuff.color.r, activebuff.color.g, activebuff.color.b, 1);
         activebuff.sprite = GetComponent<Image>().sprite;
     }
     public void SlowingBullets() {
-        foreach(var i in shooting) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
+        foreach (var i in shooting) {
             i.slow = true;
         }
         upgradePanel.active = false;
@@ -131,7 +94,9 @@ public class Upgrade : MonoBehaviour
         activebuff.sprite = GetComponent<Image>().sprite;
     }
     public void ExplodingBullets() {
-        foreach(var i in shooting) {
+        SwitchUpgrade();
+        if (!switchupgrade) return;
+        foreach (var i in shooting) {
             i.explode = true;
         }
         upgradePanel.active = false;
@@ -148,7 +113,7 @@ public class Upgrade : MonoBehaviour
         activebuff = GameObject.FindGameObjectWithTag("Buff").GetComponent<Image>();
         activebuff.color = new Color(activebuff.color.r, activebuff.color.g, activebuff.color.b, 0);
     }
-
+    public void SwitchUpgrade() { switchupgrade = !switchupgrade; }
     // Update is called once per frame
     void Update()
     {
