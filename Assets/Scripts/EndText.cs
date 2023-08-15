6b5e3fe2c26135f5ff67e63bfff63244629fc2e4
spindlyjohnny <saifulfarhan05@gmail.com>
2023-08-15 12:22:50 +0800

    ending stats

---
+++ Assets/Scripts/EndText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class EndText : MonoBehaviour
+{
+    public Text timetext;
+    public Text parttext;
+    public GameObject holder;
+    public VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        holder.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(video.isPlaying);
+        if (!video.isPlaying) holder.SetActive(true);
+        timetext.text = PlayerPrefs.GetFloat("TotalTime").ToString();
+        parttext.text = PlayerPrefs.GetFloat("TotalParts").ToString();
+    }
+}
